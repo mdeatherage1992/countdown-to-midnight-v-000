6b0ae9f496b0e8eddfe89feda23dff0d745daed8
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 17:39:03 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(x)
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(x)
+  sleep 5
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
